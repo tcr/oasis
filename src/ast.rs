45a618d74e68b4286ab845a00bb7be08bce15faf
Tim Ryan <tim@timryan.org>
2016-07-11 12:07:14 -0400

    Loads operators into scope memory.

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,63 @@
+use std::mem;
+use std::fmt;
+use std::hash::{Hash, Hasher};
+
+pub struct FnHolder {
+    f: &'static Fn(Expr, Expr) -> Expr,
+}
+
+impl FnHolder {
+    pub fn new(f: &'static Fn(Expr, Expr) -> Expr) -> FnHolder {
+        FnHolder {
+            f: f,
+        }
+    }
+
+    fn id(&self) -> [usize; 2] {
+        unsafe {
+            mem::transmute(self.f)
+        }
+    }
+}
+
+impl Hash for FnHolder {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        for item in self.id().iter() {
+            state.write_usize(*item)
+        }
+    }
+}
+
+impl PartialEq for FnHolder {
+    fn eq(&self, other: &FnHolder) -> bool {
+        self.id() == other.id()
+    }
+}
+
+impl fmt::Debug for FnHolder {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "FnHolder({:X} {:X})", self.id()[0], self.id()[1])
+    }
+}
+
+impl Eq for FnHolder {}
+
 #[derive(Debug, Eq, PartialEq, Hash)]
 pub enum Expr {
     Int(i32),
     Atom(String),
     SExpr(Vec<Box<Expr>>),
-    //Func(Box<Fn(Expr) -> Expr>),
+    Func(FnHolder),
+}
+
+impl Expr {
+    pub fn call(&self, a: Expr, b: Expr) -> Expr {
+        match self {
+            &Expr::Func(ref holder) => {
+                let func = &holder.f;
+                func(a, b)
+            }
+            _ => unreachable!(),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// TODO https://github.com/ivanjovanovic/sicp/blob/master/2.3/2.3-binary-trees.scm
+
 pub mod lisp;
 pub mod ast;
 
@@ -19,19 +21,18 @@ impl Scope {
         })))
     }
 
-    pub fn lookup<F>(&self, key: &Expr, inner: F) -> bool
-    where F: Fn(Option<&Expr>) {
+    pub fn lookup<F, T>(&self, key: &Expr, mut inner: F) -> Option<T>
+    where F: FnMut(Option<&Expr>) -> T {
         match self.scope.get(key) {
             Some(ref value) => {
-                inner(Some(value));
-                true
+                Some(inner(Some(value)))
             }
             None => {
                 match self.parent {
                     Some(ref parent) => {
                         parent.borrow().lookup(key, inner)
                     }
-                    None => false,
+                    None => None,
                 }
             }
         }
@@ -45,12 +46,16 @@ fn eval_add(a: Expr, b: Expr) -> Expr {
     })
 }
 
+static EVAL_ADD: fn(Expr, Expr) -> Expr = eval_add;
+
 fn eval_sub(a: Expr, b: Expr) -> Expr {
     Expr::Int(match (a, b) {
         (Expr::Int(a), Expr::Int(b)) => a - b,
         _ => 0
     })
 }
+
+static EVAL_SUB: fn(Expr, Expr) -> Expr = eval_sub;
 
 fn eval_mul(a: Expr, b: Expr) -> Expr {
     Expr::Int(match (a, b) {
@@ -59,6 +64,8 @@ fn eval_mul(a: Expr, b: Expr) -> Expr {
     })
 }
 
+static EVAL_MUL: fn(Expr, Expr) -> Expr = eval_mul;
+
 fn eval_div(a: Expr, b: Expr) -> Expr {
     Expr::Int(match (a, b) {
         (Expr::Int(a), Expr::Int(b)) => a / b,
@@ -66,19 +73,25 @@ fn eval_div(a: Expr, b: Expr) -> Expr {
     })
 }
 
+static EVAL_DIV: fn(Expr, Expr) -> Expr = eval_div;
+
 fn eval_expr(scope: &Scope, x: Expr, args: Vec<Box<Expr>>) -> Expr {
     use ast::Expr::*;
     let mut args: Vec<Expr> = args
         .into_iter()
         .map(|x| scope.eval(*x, eval_expr))
         .collect();
+
     match x {
-        Atom(kind) => match kind.as_ref() {
-            "+" => eval_add(args.remove(0), args.remove(0)),
-            "-" => eval_sub(args.remove(0), args.remove(0)),
-            "*" => eval_mul(args.remove(0), args.remove(0)),
-            "/" => eval_div(args.remove(0), args.remove(0)),
-            _ => unreachable!(),
+        Atom(..) => {
+            scope.lookup(&x, move |value| {
+                match value {
+                    Some(value) => {
+                        value.call(args.remove(0), args.remove(0))
+                    }
+                    None => unreachable!(),
+                }
+            }).unwrap()
         },
         _ => unreachable!(),
     }
@@ -102,7 +115,14 @@ fn main() {
 
     let s = Scope::new(None);
     let s2 = Scope::new(Some(s.clone()));
-    s.borrow_mut().scope.insert(Expr::Atom("true".to_owned()), Expr::Int(1));
+    {
+        let mut s = s.borrow_mut();
+        s.scope.insert(Expr::Atom("true".to_owned()), Expr::Int(1));
+        s.scope.insert(Expr::Atom("+".to_owned()), Expr::Func(FnHolder::new(&EVAL_ADD as &'static _)));
+        s.scope.insert(Expr::Atom("-".to_owned()), Expr::Func(FnHolder::new(&EVAL_SUB as &'static _)));
+        s.scope.insert(Expr::Atom("*".to_owned()), Expr::Func(FnHolder::new(&EVAL_MUL as &'static _)));
+        s.scope.insert(Expr::Atom("/".to_owned()), Expr::Func(FnHolder::new(&EVAL_DIV as &'static _)));
+    }
     s2.borrow().lookup(&Expr::Atom("true".to_owned()), |expr| {
         println!("lookup {:?}", expr);
     });
