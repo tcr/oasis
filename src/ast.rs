d56606329cf44a266e9e1d137f55cac2a28c29ef
Tim Ryan <tim@timryan.org>
2016-07-10 23:36:16 -0400

    Working Lisp.

--- src/ast.rs
+++ src/ast.rs
@@ -4,3 +4,16 @@ pub enum Expr {
     Atom(String),
     SExpr(Vec<Box<Expr>>),
 }
+
+impl Expr {
+    pub fn eval<F>(self, inner: F) -> Expr
+    where F: Fn(Expr, Vec<Box<Expr>>) -> Expr {
+        match self {
+            Expr::SExpr(mut args) => {
+                let term = args.remove(0);
+                inner(*term, args)
+            },
+            _ => self,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,55 @@
 pub mod lisp; // synthesized by LALRPOP
 pub mod ast;
 
+use ast::*;
+
+fn eval_add(a: Expr, b: Expr) -> Expr {
+    Expr::Int(match (a, b) {
+        (Expr::Int(a), Expr::Int(b)) => a + b,
+        _ => 0
+    })
+}
+
+fn eval_sub(a: Expr, b: Expr) -> Expr {
+    Expr::Int(match (a, b) {
+        (Expr::Int(a), Expr::Int(b)) => a - b,
+        _ => 0
+    })
+}
+
+fn eval_mul(a: Expr, b: Expr) -> Expr {
+    Expr::Int(match (a, b) {
+        (Expr::Int(a), Expr::Int(b)) => a * b,
+        _ => 0
+    })
+}
+
+fn eval_div(a: Expr, b: Expr) -> Expr {
+    Expr::Int(match (a, b) {
+        (Expr::Int(a), Expr::Int(b)) => a / b,
+        _ => 0
+    })
+}
+
+fn eval_expr(x: Expr, args: Vec<Box<Expr>>) -> Expr {
+    use ast::Expr::*;
+    let mut args: Vec<Expr> = args.into_iter().map(|x| x.eval(eval_expr)).collect();
+    match x {
+        Atom(kind) => match kind.as_ref() {
+            "+" => eval_add(args.remove(0), args.remove(0)),
+            "-" => eval_sub(args.remove(0), args.remove(0)),
+            "*" => eval_mul(args.remove(0), args.remove(0)),
+            "/" => eval_div(args.remove(0), args.remove(0)),
+            _ => unreachable!(),
+        },
+        _ => unreachable!(),
+    }
+}
+
 fn main() {
-    println!("{:?}", lisp::parse_Exprs("(+ (* 22 44) 66)").unwrap());
+    let mut parse = lisp::parse_Exprs("(+ (* 22 44) 66)").unwrap();
+
+    let res = parse.remove(0).eval(eval_expr);
+
+    println!("{:?}", res);
 }
